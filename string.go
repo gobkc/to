8e5c9a7cd133685f42cd70d6e0dfa6dd1ebece91
xiongjianbo <jianbo.xiong@target-energysolutions.com>
2024-01-12 18:02:44 +0800

    optimize Json function

--- string.go
+++ string.go
@@ -10,6 +10,31 @@ func String(dest any) string {
 }
 
 func Json(dest any) string {
-	b, _ := json.MarshalIndent(dest, "", "\t")
-	return string(b)
+	switch v := dest.(type) {
+	case string:
+		var jsonObj map[string]interface{}
+		err := json.Unmarshal([]byte(v), &jsonObj)
+		if err != nil {
+			return fmt.Sprintf("Error unmarshalling JSON string: %v", err)
+		}
+
+		b, err := json.MarshalIndent(jsonObj, "", "\t")
+		if err != nil {
+			return fmt.Sprintf("Error marshalling JSON: %v", err)
+		}
+		return string(b)
+
+	case []byte:
+		return Json(string(v))
+
+	case nil:
+		return ""
+
+	default:
+		b, err := json.MarshalIndent(dest, "", "\t")
+		if err != nil {
+			return fmt.Sprintf("Error marshalling JSON: %v", err)
+		}
+		return string(b)
+	}
 }
