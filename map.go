c0affc8f0d905324f330916453fb069b1a7ee232
xiongjianbo <jianbo.xiong@target-energysolutions.com>
2024-10-14 17:50:56 +0800

    added List2Maps api

--- map.go
+++ map.go
@@ -9,3 +9,17 @@ func List2Map[T any, Filed comparable](list []T, callback func(T) Filed) (map[Fi
 	}
 	return m, nil
 }
+
+// List2Maps list convert to maps
+func List2Maps[T any, Filed comparable](list []T, callback func(T) Filed) (map[Filed][]T, error) {
+	m := make(map[Filed][]T)
+	for _, t := range list {
+		field := callback(t)
+		if q, ok := m[field]; ok {
+			m[field] = append(q, t)
+		} else {
+			m[field] = []T{t}
+		}
+	}
+	return m, nil
+}
